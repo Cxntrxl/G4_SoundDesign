6979069f70c0b2d28baeb9c5fafadff2a6a67bf6
Cxntrxl <jacksonwood2905@gmail.com>
2023-07-19 11:50:02 +1000

    #3 / #4 - Cooldown Finish [P1 / P2]

--- Assets/Script/CoolDownBars.cs
+++ Assets/Script/CoolDownBars.cs
@@ -11,12 +11,20 @@ public class CoolDownBars : MonoBehaviour
     public Slider player1CoolDown;
     public Slider player2CoolDown;
 
+    public AudioSource P1_CooldownComplete;
+    public AudioSource P2_CooldownComplete;
+    public bool P1_CooldownPlayed = true;
+    public bool P2_CooldownPlayed = true;
+
     private float p1CoolDown;
     private float p2CoolDown;
 
     // Start is called before the first frame update
     void Start()
     {
+        P1_CooldownComplete = GameObject.Find("UI_P1_CooldownComplete_audioSource").GetComponent<AudioSource>();
+        P2_CooldownComplete = GameObject.Find("UI_P2_CooldownComplete_audioSource").GetComponent<AudioSource>();
+
         player2Controller = GameObject.Find("Player2").GetComponent<Player2Controller>();
         player1Attacks = GameObject.Find("Player1").GetComponent<PlayerOneAttacks>();
 
@@ -32,5 +40,25 @@ public class CoolDownBars : MonoBehaviour
 
         player1CoolDown.value = p1CoolDown;
         player2CoolDown.value = p2CoolDown;
+
+        if (p1CoolDown == 2 && !P1_CooldownPlayed)
+        {
+            P1_CooldownComplete.Play();
+            P1_CooldownPlayed = true;
+        }
+        else if (p1CoolDown != 2)
+        {
+            P1_CooldownPlayed = false;
+        }
+
+        if (p2CoolDown == 5 && !P2_CooldownPlayed)
+        {
+            P2_CooldownComplete.Play();
+            P2_CooldownPlayed = true;
+        }
+        else if (p2CoolDown != 5)
+        {
+            P2_CooldownPlayed = false;
+        }
     }
 }
