221a2d33906b24b669167b601b181420ea2bca96
Cxntrxl <jacksonwood2905@gmail.com>
2023-07-20 09:40:59 +1000

    Fixed a bug where audio settings sliders would not save their values upon swapping scenes

--- Assets/Script/sounds/volumeSliders.cs
+++ Assets/Script/sounds/volumeSliders.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class volumeSliders : MonoBehaviour
 {
     public AudioMixer mixer;
     public string soundType;
-    
+
+    private void Start()
+    {
+        float soundLevel;
+        mixer.GetFloat(soundType, out soundLevel);
+        this.GetComponent<Slider>().value = soundLevel;
+    }
+
     public void SetSound(float soundLevel)
     {
         mixer.SetFloat(soundType, soundLevel);
