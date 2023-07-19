7c25c145c517044fb1d346954abf152e0238414e
Cxntrxl <jacksonwood2905@gmail.com>
2023-07-19 12:19:53 +1000

    #5 - Death Sound

--- Assets/Script/HeathBars.cs
+++ Assets/Script/HeathBars.cs
@@ -11,12 +11,16 @@ public class HeathBars : MonoBehaviour
     private float p1Health;
     private float p2Health;
 
+    public AudioSource PX_DeathSound;
+    public bool deathPlayed = false;
+
     public player2Health player2Health;
     public PlayerOneHeath player1Health;
 
     // Start is called before the first frame update
     void Start()
     {
+        PX_DeathSound = GameObject.Find("PX_DeathSound_audioSource").GetComponent<AudioSource>();
         player2Health = GameObject.Find("Player2").GetComponent<player2Health>();
         player1Health = GameObject.Find("Player1").GetComponent<PlayerOneHeath>();
 
@@ -32,5 +36,11 @@ public class HeathBars : MonoBehaviour
 
         p1HelthBar.value = p1Health;
         p2HelthBar.value = p2Health;
+
+        if ((p1HelthBar.value <= 0 || p2HelthBar.value <= 0) && !deathPlayed)
+        {
+            PX_DeathSound.Play();
+            deathPlayed = true;
+        }
     }
 }
